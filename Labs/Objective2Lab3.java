cbc219b713e94ed55a5c3f760a2dbb3a4b91aca6
Tiara <tialwright96@gmail.com>
2023-02-11 09:58:29 -0600

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,17 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+int year_born;
+year_born = 1996;
+String month_born;
+month_born = "April";
+boolean weekend = true;
+
+System.out.println(year_born);
+System.out.println(month_born); 
+System.out.println(weekend);
+
+}
+    
+  }
+

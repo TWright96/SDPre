8abf7eaf76ee3f4709b7d4ad1c3b166c7f5ce243
Tiara <tialwright96@gmail.com>
2023-02-11 15:39:17 -0600

    Objective4Lab1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("What is your first name?");
+    String fname = keyboard.nextLine();
+    System.out.println("What is your last name?");
+    String lname = keyboard.nextLine();
+    System.out.println("What is your favorite animal?");
+    String favoriteAnimal = keyboard.nextLine();
+    System.out.println("What is your favorite food?");
+    String favoriteFood = keyboard.nextLine();
+    System.out.println("What is your favorite song?");
+    String favoriteSong = keyboard.nextLine();
+
+   
+
+    //TODO print the output formatted to look like the expected output using String concatenation.
+
+  }
+}

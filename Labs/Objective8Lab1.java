415ea52c916344af154cea1683d1de7f5b51f546
Tiara <tialwright96@gmail.com>
2023-02-13 13:05:39 -0600

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,20 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int t;
+
+    for (t = 1; t <= 10; t++) {
+
+      System.out.println(t);
+
+    }
+    for (t=10; t >= 1; t--) {
+      
+      System.out.println(t);
+    }
+    //TODO Write a for loop that will print the numbers 1-10
+
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
+}

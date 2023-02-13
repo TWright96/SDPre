1e816e1f0839749f865d5752408ac53e22437dce
Tiara <tialwright96@gmail.com>
2023-02-13 13:57:20 -0600

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,16 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+   int t, sum;
+   sum = 0;
+
+   for(t = 1; t <= 20; t++) {
+    
+    sum = sum + t;
+   
+    
+   }
+   System.out.println(sum);
+
+
+	}
+}

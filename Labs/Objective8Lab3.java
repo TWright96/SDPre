ee954c09e1d07eead363d1e7fb3a587f12a7e9a1
Tiara <tialwright96@gmail.com>
2023-02-13 21:10:21 -0600

    Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3 {
+	public static void main(String[] args) {
+          int t;
+
+		for(t = 1; t <= 20; t++) {
+
+			if(t % 2 != 0) {
+				System.out.println(t + " is odd");
+			}
+
+			if(t % 2 == 0) {
+				System.out.println(t + " is even");			}
+			}
+		}
+	}
